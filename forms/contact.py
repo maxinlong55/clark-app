ca82d48dc225caa00590cef092522b75cfefbecc
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-01 14:43:02 +0800

    Create contact.py

---
+++ forms/contact.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def contact_form():
+	with st.form("contact_form"):
+		name=st.text_input("First Name")
+		email = st.text_input("Email Address")
+		message=st.text_area("You Message")
+		submit_button=st.form_submit_button("Submit")
+
+		if submit_button:
+			st.success("Message successfully sent")
