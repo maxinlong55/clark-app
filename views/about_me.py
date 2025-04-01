f833ac93c935e62568144b14268fd975a97a51e6
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-01 13:59:32 +0800

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -11,3 +11,21 @@ with col2:
 		- A little knowledge about Python
 		"""
 	)
+
+st.write("\n")
+st.subheader("Experience & Qualifications", anchor=False)
+st.write(
+	"""
+	- 2 Years maintain experience of hydraulic system
+	- 7 Years tail-made pressure test equipment design
+	- 8 Years Paper machine hydraulic design """
+)
+
+st.write("\n")
+st.subheader("Hard skill", anchor=False)
+st.write(
+	"""
+	- Tools: Auto Cads, Invent, AmeSim
+	- Design scope: hydraulic,Pneumatic,Lubrication
+	- Scope:Fluid design \ Workshop test \ Commissioning """
+)
