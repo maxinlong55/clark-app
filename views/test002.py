6d5065680b2e4b2c7a2620e7c197dcccd501b6ad
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-01 10:47:31 +0800

    Update test002.py

--- views/test002.py
+++ views/test002.py
@@ -2,3 +2,8 @@ import streamlit as st
 st.title("third")
 if st.button("ballons"):
   st.balloons()
+
+enable = st.checkbox("Enable Camera")
+picture=st.camera_input("take a picture",disabled= not enable)
+if picture:
+	st.image(picture)
