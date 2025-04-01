1cae2304583d0f584809802359202dd4913ac69f
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-01 14:08:08 +0800

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -4,7 +4,7 @@ col1,col2 = st.columns(2,gap="small",vertical_alignment="center")
 with col1:
 	st.image("./assets/profile_image.png",width=230)
 with col2:
-	st.title("Clark Ma",anchor=True)
+	st.title("Clark Ma")
 	st.write(
 		"""
 		- Hydraulic engineer.
