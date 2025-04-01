5b7de91baf5ff827323fad37377a95a5848f055b
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-01 13:43:45 +0800

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write("this is all about me")
+
+col1,col2 = st.columns(2,gap="small",vertical_alignment="center")
+with col1:
+	st.image("./assets/profile_image.png",width=230)
+with col2:
+	st.title("Clark Ma",anchor=False)
+	st.write("Hydraulic engineer")
+	st.write("A little knowledge about Python")
