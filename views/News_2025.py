62e792db90c21e895c3c89e83de6ac65c587ff12
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-02 14:47:57 +0800

    Update News_2025.py

--- views/News_2025.py
+++ views/News_2025.py
@@ -2,3 +2,8 @@ import streamlit as st
 st.title("News and Comments")
 st.markdown("hello, this is markdown")
 st.write("hello, this is write")
+st.markdown("""
+第一行文字（默认左对齐）  
+<span style="text-align: center; display:block">第二行文字（居中）</span>  
+第三行文字（默认左对齐）
+""", unsafe_allow_html=True)
