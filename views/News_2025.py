e9e39d18d8a73a908d974e93a93045349301097f
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-02 16:30:21 +0800

    Update News_2025.py

--- views/News_2025.py
+++ views/News_2025.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-col1,col2,col3 = st.columns(3,gap="small",vertical_alignment="center")
-with col2:
-	st.markdown("News and Comments")
-
-st.subheader("<h2 style='text-align: center;'>News and Comments</h2>")
+st.markdown(
+    """<p style="text-align: center; font-size: 24px;">News and Comments（24px）</p>""",
+    unsafe_allow_html=True
+)
 
 st.markdown("""News and Comments""",unsafe_allow_html=True)
 st.markdown("mark_donw")
