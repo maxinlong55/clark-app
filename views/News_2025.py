9e0a6d5bc63e0b6dc11d39a7dd641d011b5af5da
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-02 16:26:25 +0800

    Update News_2025.py

--- views/News_2025.py
+++ views/News_2025.py
@@ -4,6 +4,8 @@ col1,col2,col3 = st.columns(3,gap="small",vertical_alignment="center")
 with col2:
 	st.markdown("News and Comments")
 
+st.subheader("<h2 style='text-align: center;'>News and Comments</h2>")
+
 st.markdown("""News and Comments""",unsafe_allow_html=True)
 st.markdown("mark_donw")
 st.title("title")
