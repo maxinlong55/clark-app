5026064ac8600a4b42d22e6abc6f10dfa4306ab9
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-02 16:30:58 +0800

    Update News_2025.py

--- views/News_2025.py
+++ views/News_2025.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.markdown(
-    """<p style="text-align: center; font-size: 24px;">News and Comments（24px）</p>""",
+    """<p style="text-align: center; font-size: 20px;">News and Comments（20px）</p>""",
     unsafe_allow_html=True
 )
 
-st.markdown("""News and Comments""",unsafe_allow_html=True)
+st.markdown("""News and Comments""")
 st.markdown("mark_donw")
 st.title("title")
 st.header("header")
