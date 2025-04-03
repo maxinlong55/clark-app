6377bb42d0bbf82e272e9f4efed8e08fc7e751a2
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-03 16:07:38 +0800

    Update chart.py

--- views/chart.py
+++ views/chart.py
@@ -4,28 +4,51 @@ import pandas as pd
 
 st.title("All kinds of Chart")
 
-chart_data = pd.DataFrame(np.random.randn(15, 3), columns=["A", "B", "C"])
+df= pd.read_csv("assets/diabetes.csv")
+df=df.head(30)
+tab1,tab2,tab3,tab4,tab5,tab6,=st.tabs(["line chart","line chart_x_y","line_in_one","chart area","bar chart","scatter chart"])
+tab1.line_chart(df["Glucose"],x_label="No meaning1",y_label="value1",color=(0,255,0),use_container_width=True) #default x is int (0,11)
+tab2.line_chart(df,x="Age",y="Glucose",x_label="No meaning2",y_label="value2",color=(255,255,0),use_container_width=False)
+tab3.line_chart(df,x_label="No meaning 3",y_label="valve3",use_container_width=True) # if we defin color, color should be a list
+tab4.area_chart(df,x_label="No meaning 4",y_label="valve4")
+tab5.bar_chart(df,x_label="No meaning 5",y_label="valve5")
+tab5.bar_chart(df["Glucose"],x_label="No meaning 5",y_label="valve5")
+tab6.scatter_chart(df,size=30,x_label="No meaning 6",y_label="valve6")
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
-	["line_chart", "Area Chart", "bar_chart", "scatter_chart", "data_frame", "code"])
-tab1.line_chart(chart_data["A"], x_label="This is x-axis", y_label="This is Y axis", use_container_width=True,
-                color=(0, 255, 0))
-tab1.line_chart(chart_data, x="A", y="B", x_label="This is x-axis", y_label="This is Y axis", use_container_width=True,
-                color=(0, 255, 255))
-tab1.line_chart(chart_data, x_label="This is x-axis", y_label="This is Y axis", use_container_width=True,
-                color=[(255, 255, 0), (0, 255, 0), (0, 0, 255)])
-tab2.subheader("Area Chart")
-tab2.area_chart(chart_data)
+st.divider()
+st.markdown("Matplotlib")
 
-tab3.bar_chart(chart_data)
-tab3.bar_chart(chart_data["B"])
 
-tab4.subheader("scatter")
-tab4.scatter_chart(chart_data)
+fig,ax = plt.subplots(3,2,figsize=(10,15))
+# plt.xkcd()
+# plt.style.use("fivethirtyeight")
+ax[0,0].scatter(range(len(df["Glucose"])),df["Glucose"],color='red')
+ax[0,0].set_title("mapltolib title")
+ax[0,0].set_xlabel("Number")
+ax[0,0].set_ylabel("value")
 
-tab5.write(chart_data)
 
-example = """def hello():
-	print('hello')
-	print("ok")"""
-tab6.code(example)
+ax[0,1].plot(range(len(df["Glucose"])),df["Glucose"],color='green',label="Glucose")
+ax[0, 1].legend(loc="upper right")
+ax[0, 1].set_title("Glucose Trend")
+ax[0, 1].set_xlabel("Index")
+ax[0, 1].set_ylabel("Glucose Level")
+
+number = 6
+myexplode = [0.2, 0, 0, 0,0,0]
+mylabels=[]
+for i in range(number):
+	mylabels.append(f"Glucose-{i}")
+
+ax[1,0].bar(mylabels,df["Glucose"].head(number))
+ax[1,0].set_xticklabels(mylabels,rotation=75)
+
+
+ax[1,1].pie(df["Glucose"].head(number),labels=mylabels,explode=myexplode,shadow = True)
+
+
+ax[2,0].barh(mylabels,df["Glucose"].head(number))
+
+# plt.legend()
+plt.tight_layout()
+st.pyplot(fig)
