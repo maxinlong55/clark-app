371979d55df8d4cacbc75094bd0da05e99b3f362
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-07 08:17:53 +0800

    Update clark-app.py

--- clark-app.py
+++ clark-app.py
@@ -23,7 +23,7 @@ News_2025=st.Page(page="views/News_2025.py",
 pg=st.navigation(
     {
         "Info":[about_me],
-        "Project":[chart,test002],
+        "Project":[chart,camera],
 	"News":[News_2025]
     }
 )
