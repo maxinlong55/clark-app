adc3e252489152ffbc54f3073bd297f42a3217f9
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-14 13:36:51 +0800

    Update and rename test.py to daily.py

---
+++ views/daily.py
@@ -0,0 +1,26 @@
+import streamlit as st
+st.markdown(
+    """<p style="text-align: center; font-size: 20px;">Gym</p>""",
+    unsafe_allow_html=True
+)
+st.markdown(
+    """<p style="text-align: center; font-size: 18px;"> April 14,2025 </p>""",
+    unsafe_allow_html=True
+)
+st.markdown(""""
+A few years ago, I met a Chinese American who ask me, why so few Chinese go to Gym?
+She told me, the gym could good for your bone and muscle. I were thinking about question, do I really
+need to go there? At that time, I felt good physically without any gym. 
+While I were on my 40s, I feel weaker year by year. My eyes fell blurry while I spend another a few hour 
+frond of my computer after a whole day front it. I began to go to gym and go to swimming.
+
+Suddenly, I realized that the reason I don't go to gym while i were younger, which is because i don't need 
+to there, and i thought i can learn something while evening. While i am older, I need some exercise and 
+I don't learn too much useful stuff on evening actually.
+
+Of course, that Chinese American go to gym at her early age, it's good for her. But while you realize its 
+necessary have it and you are relatively free, that's not to late to pick it up.
+
+We are not necessary on the same knowledge level. Just be better yourself comparing with yesterday.
+""")
+st.divider()
