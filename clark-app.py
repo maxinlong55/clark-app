f16fc1e5e4be6f8f33cfa5df9eda6f3ec33c38bf
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-14 13:36:03 +0800

    Update clark-app.py

--- clark-app.py
+++ clark-app.py
@@ -19,8 +19,8 @@ camera=st.Page(page="views/camera.py",
 News_2025=st.Page(page="views/News_2025.py",
                 title="News 2025 ",
                 icon=":material/newspaper:")
-TEST=st.Page(page="views/test.py",
-                title="TEST ",
+daily=st.Page(page="views/daily.py",
+                title="Daily",
                 icon=":material/robot:")
 
 pg=st.navigation(
@@ -28,7 +28,7 @@ pg=st.navigation(
         "Info":[about_me],
         "Project":[chart,camera],
         "News":[News_2025],
-        "TEST":[TEST],
+        "Daily":[daily],
     }
 )
 
