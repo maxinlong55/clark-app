4711df672347de81392ebacb05b6a35050bfddd9
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-15 22:09:28 +0800

    Update daily.py

--- views/daily.py
+++ views/daily.py
@@ -5,8 +5,26 @@ st.markdown(
 )
 st.divider()
 
+st.markdown(
+    """<p style="text-align: center; font-size: 20px;">Don't complai</p>""",
+    unsafe_allow_html=True
+)
+st.markdown(
+    """<p style="text-align: center; font-size: 18px;"> April 15,2025 </p>""",
+    unsafe_allow_html=True
+)
+st.markdown("""
+Life is good for me actually. I never can be the best as I expected. 
+Base on my family and school education, and intellectual and physical ability, I absolutely surpass
+over 75% of my peers. I lose something, I also gain something. People also can't get all their wanted.
+We already regret something. But while you get older, don't let it make your unhappy. 
+There were have some good moments, there were some bad moments. While you compare with others, 
+If you are above the middle level, don't be too much cynical.
+Life is too short to be sad. 
+""")
 
 
+st.divider()
 st.markdown(
     """<p style="text-align: center; font-size: 20px;">Gym</p>""",
     unsafe_allow_html=True
