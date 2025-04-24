88a37bc6efeeb6c83e4b2b0b9463b8b1a39de4cd
maxinlong55 <63723657+maxinlong55@users.noreply.github.com>
2025-04-24 15:43:35 +0800

    Update camera.py

--- views/camera.py
+++ views/camera.py
@@ -2,7 +2,6 @@ import streamlit as st
 import cv2
 import numpy as np
 
-
 st.title("Camera with OpenCV Processing")
 enable = st.checkbox("Enable Camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
